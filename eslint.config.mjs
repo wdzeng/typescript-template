472f69e2ed01e1a828ad06b3af511e2dc376ff6d
hyperbola <me@hyperbola.me>
2025-03-27 19:04:47 +0800

    style: update eslint.config.mjs to use vitest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,6 @@ export default getConfigForTs(
   // Options
   {
     browser: false, // Whether in browser
-    ecmaVersion: 2022, // ECMAScript version
-    ignores: ['build', 'dist', 'out'], // Ignore files
-    node: true, // Whether in Node.js
-    projectRoot: import.meta.dirname // Project root dir
+    vitest: true // Whether to use Vitest
   }
 )
