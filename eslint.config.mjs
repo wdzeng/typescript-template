54129af84c8d381c10d5fbc7560acb603c69aa59
hyperbola <me@hyperbola.me>
2025-03-27 19:05:03 +0800

    chore: update eslint.config.mjs field order
    Just to make it prettier.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,10 @@ export default getConfigForTs(
   {},
   // Options
   {
+    projectRoot: import.meta.dirname, // Project root directory
+    ecmaVersion: 2023, // ECMAScript version
+    ignores: ['build', 'dist', 'out'], // Global ignore files
+    node: true, // Whether in Node.js
     browser: false, // Whether in browser
     vitest: true // Whether to use Vitest
   }
